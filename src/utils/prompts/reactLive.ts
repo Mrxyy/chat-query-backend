169838d29809b10e6609e1b1f0326547fbd813bb
熊洋洋 <17362864761@163.com>
2023-06-27 22:20:56 +0800

    fix: fix reactview ansager answer

--- src/utils/prompts/reactLive.ts
+++ src/utils/prompts/reactLive.ts
@@ -12,10 +12,28 @@ export const defaultScope = `
 {
   data,
   import: {
-    'styled-components':
-      'A css in js Framework,Visual primitives for the component age. Use the best bits of ES6 and CSS to style your apps without stress',
-    'echarts-for-react':
-      'The simplest, and the best React wrapper for Apache ECharts.',
+    'styled-components':{
+      '描述':'A css in js Framework,Visual primitives for the component age. Use the best bits of ES6 and CSS to style your apps without stress'
+    },
+    'echarts':{
+      '描述'：'Apache ECharts is a powerful, interactive charting and data visualization library for browser.',
+      '用法': "import echarts from 'echarts';"
+    },
+    'echarts-for-react':{
+      '描述'：'Apache ECharts is a powerful, interactive charting and data visualization library for browser.',
+      '用法': "import React from 'react';
+import ReactECharts from 'echarts-for-react';  // or var ReactECharts = require('echarts-for-react');
+
+<ReactECharts
+  option={this.getOption()}
+  notMerge={true}
+  lazyUpdate={true}
+  theme={"theme_name"}
+  onChartReady={this.onChartReadyCallback}
+  onEvents={EventsDict}
+  opts={}
+/>"
+    },
   },
 }
 `;
@@ -29,7 +47,7 @@ const Messages_1 =
   HumanMessagePromptTemplate.fromTemplate(`我正在使用react-live作为一个实时编辑和编译React组件的工具。请根据我的需求，输出一段能在react-live运行的代码，只需要导出默认组件，不用挂载dom。可用的作用域如下：
 {scope}
 
-其中import包含可以导入的库,请不要使用任何其他第三方库。 key为包名，value 是包的描述。其它每个属性将作为单独全局变量，变量名为属性名。请确保默认导出的组件接受null作为参数，并将传入props取自模块全局作用域变量data（const props = data）。例如：
+其中import可供导入的库,除此之外不能使用其它第三方库。 key为包名，value 是包的描述。其它每个属性将作为单独全局变量，变量名为属性名。请确保默认导出的组件接受null作为参数，并将传入props取自模块全局作用域变量data（const props = data）。例如：
 
 {fxTepmlate}
 `);
