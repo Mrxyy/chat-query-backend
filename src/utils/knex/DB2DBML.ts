63b813652297e8980fd16b7447ae4dec63766647
jaden <49010414+Mrxyy@users.noreply.github.com>
2023-07-07 10:56:21 +0800

    fix: fix db version/type feild

--- src/utils/knex/DB2DBML.ts
+++ src/utils/knex/DB2DBML.ts
@@ -19,7 +19,8 @@ export async function generateDbdiagramDsl(
 
   let dsl = '';
 
-  for (const { TABLE_NAME: table_name } of tables) {
+  for (const table of tables) {
+    const table_name = table['TABLE_NAME'] || table['table_name'];
     dsl += `Table ${table_name} {\n`;
 
     // console.log(databaseName,table_name)
@@ -56,12 +57,14 @@ export async function generateDbdiagramDsl(
           .andWhereRaw('referenced_table_name IS NOT NULL');
 
     const flag: Record<string, any> = {};
-    for (const {
-      COLUMN_NAME: column_name,
-      REFERENCED_TABLE_NAME: referenced_table_name,
-      REFERENCED_COLUMN_NAME: referenced_column_name,
-      DATA_TYPE: data_type,
-    } of foreignKeys) {
+    for (const item of foreignKeys) {
+      const column_name = item['COLUMN_NAME'] || item['column_name'];
+      const referenced_table_name =
+        item['REFERENCED_TABLE_NAME'] || item['referenced_table_name'];
+      const referenced_column_name =
+        item['REFERENCED_COLUMN_NAME'] || item['referenced_column_name'];
+      const data_type = item['DATA_TYPE'] || item['data_type'];
+
       if (isOracle) {
         const refInfo = await knexInstance('all_constraints')
           .select('table_name', 'column_name')
@@ -74,20 +77,22 @@ export async function generateDbdiagramDsl(
         }
       } else {
         const columnInfo = columns.find(
-          (column) => column.COLUMN_NAME === column_name,
+          (column) =>
+            (column.COLUMN_NAME || column.column_name) === column_name,
         );
-        const data_type = columnInfo ? columnInfo.DATA_TYPE : '';
+        const data_type = columnInfo
+          ? columnInfo.DATA_TYPE || columnInfo.data_type
+          : '';
         // 处理外键关系的代码
         dsl += `  ${column_name} ${data_type.toUpperCase()} [ref: > ${referenced_table_name}.${referenced_column_name}]\n`;
       }
       flag[column_name] = true;
     }
 
-    for (const {
-      COLUMN_NAME: column_name,
-      DATA_TYPE: data_type,
-      COLUMN_KEY: column_key,
-    } of columns) {
+    for (const col of columns) {
+      const column_name = col.COLUMN_NAME || col.column_name;
+      const data_type = col.DATA_TYPE || col.data_type;
+      const column_key = col.COLUMN_KEY || col.column_key;
       if (!flag[column_name])
         dsl += `  ${column_name} ${data_type.toUpperCase()}${
           column_key === 'PRI' ? ' [pk]' : ''
